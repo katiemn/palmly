339488c12b544704ad1e939dc4e170dcd2fbb7c3
mtollive <mtollive@lion.lmu.edu>
2020-03-22 22:51:14 -0700

    add skeleton tabs

---
+++ palmly-app/palmly-app/Lifestyle.swift
@@ -0,0 +1,21 @@
+//
+//  Lifestyle.swift
+//  palmly-app
+//
+//  Created by Vincent Fitzgerald Tolliver on 3/22/20.
+//  Copyright © 2020 Katie Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct Lifestyle: View {
+    var body: some View {
+        Text("My Lifestyle")
+    }
+}
+
+struct Lifestyle_Previews: PreviewProvider {
+    static var previews: some View {
+        Lifestyle()
+    }
+}
---
+++ palmly-app/palmly-app/Personality.swift
@@ -0,0 +1,21 @@
+//
+//  Personality.swift
+//  palmly-app
+//
+//  Created by Vincent Fitzgerald Tolliver on 3/22/20.
+//  Copyright © 2020 Katie Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct Personality: View {
+    var body: some View {
+        Text("Hello, Personality!")
+    }
+}
+
+struct Personality_Previews: PreviewProvider {
+    static var previews: some View {
+        Personality()
+    }
+}
