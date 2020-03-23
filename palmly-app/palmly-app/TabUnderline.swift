547a8bb31d91a0a9f43095a78a6a310d0f2a902e
mtollive <mtollive@lion.lmu.edu>
2020-03-23 00:56:46 -0700

    add top bar divider and navigation to tabbed pages

---
+++ palmly-app/palmly-app/TabUnderline.swift
@@ -0,0 +1,21 @@
+//
+//  TabUnderline.swift
+//  palmly-app
+//
+//  Created by Vincent Fitzgerald Tolliver on 3/23/20.
+//  Copyright © 2020 Katie Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct TabUnderline: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabUnderline_Previews: PreviewProvider {
+    static var previews: some View {
+        TabUnderline()
+    }
+}
