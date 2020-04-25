7b5b48d45ba3071e1b23ff551b1c4caf06315c64
mtollive <mtollive@lion.lmu.edu>
2020-04-25 16:49:07 -0700

    move view routing files to top  file

---
+++ palmly-app/palmly-app/Header.swift
@@ -0,0 +1,50 @@
+//
+//  Header.swift
+//  palmly-app
+//
+//  Created by Vincent Fitzgerald Tolliver on 3/29/20.
+//  Copyright © 2020 Katie Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct Header: View {
+    
+//    var title: String
+//    @ObservedObject var viewRouter: ViewRouter
+    
+    var body: some View {
+        Text("Welcome to the machine")
+//        VStack(alignment: .leading) {
+//            Button(action: {
+//                if self.viewRouter.currentPage > 0 {
+//                    self.viewRouter.currentPage -= 1
+//                }
+//                }) {
+//                Image("left arrow")
+//                    .foregroundColor(Color.black)
+//            }
+//            Rectangle()
+//                .fill(Color.black)
+//                .frame(width: 380, height: 5)
+//
+//            Text(self.title)
+//                .font(.custom("ZillaSlab-SemiBold", size: 48))
+//
+//            Rectangle()
+//            .fill(Color.white)
+//            .frame(width: 380, height: 10)
+//
+//            Rectangle()
+//            .fill(topDividerGrey)
+//            .frame(width: 380, height: 2)
+//        }
+    }
+}
+
+struct Header_Previews: PreviewProvider {
+    static var previews: some View {
+//        Header(title: "Your Reading", viewRouter: ViewRouter())
+        Header()
+    }
+}
---
+++ palmly-app/palmly-app/TabUnderline.swift
@@ -0,0 +1,28 @@
+//
+//  TabUnderline.swift
+//  palmly-app
+//
+//  Created by Vincent Fitzgerald Tolliver on 3/23/20.
+//  Copyright © 2020 Katie Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct TabUnderline: View {
+    var body: some View {
+        Text("Take Photo View Controller")
+//        GeometryReader{ geometry in
+//            Rectangle()
+//                .fill(topDividerGrey)
+//                .frame(width: geometry.size.width - 20, height: 5)
+//                .offset(y: -350)
+//
+//        }
+    }
+}
+
+struct TabUnderline_Previews: PreviewProvider {
+    static var previews: some View {
+        TabUnderline()
+    }
+}
